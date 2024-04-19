d0120098b69154e3dd1cafa8a1db23c1921f9842
isabella <115096194+sevenf0ld@users.noreply.github.com>
2024-04-19 11:24:21 +0800

    implement templates

---
+++ ex00/include/whatever.hpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maiman-m <maiman-m@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 08:28:31 by maiman-m          #+#    #+#             */
+/*   Updated: 2024/04/19 10:29:30 by maiman-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#define AC_BLACK "\x1b[30m"
+#define AC_RED "\x1b[31m"
+#define AC_GREEN "\x1b[32m"
+#define AC_YELLOW "\x1b[33m"
+#define AC_BLUE "\x1b[34m"
+#define AC_MAGENTA "\x1b[35m"
+#define AC_CYAN "\x1b[36m"
+#define AC_WHITE "\x1b[37m"
+#define AC_NORMAL "\x1b[m"
+#define AC_BOLD "\033[1m"
+#define AC_ITALIC "\033[3m"
+
+#define FORMAT_TEST(str) std::cout << AC_BOLD << AC_ITALIC << str << AC_NORMAL << std::endl;
+#define EQUAL_TEST 1
+
+#include <iostream>
+
+// function templates can be called with any argument type
+// both arguments must have the same type
+// both arguments must support all relational operators
+// templates must be defined in the header file
+
+// swap 
+// swap the two arguments, returns void
+template <typename T>
+void swap(T &a, T &b) // fn template definition for swap<T>
+{
+	T tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+// min
+// returns the smaller of the two arguments, returns the second if equal
+template <typename T>
+T min(const T &a, const T &b) // fn template definition for min<T>
+{
+	if (EQUAL_TEST)
+		std::cout << AC_YELLOW << "(" << (a < b ? &a : &b) << ") " << AC_NORMAL << '\t';
+	return (a < b ? a : b);
+}
+
+// max
+// returns the greater of the two arguments, returns the second if equal
+template <typename T>
+T max(const T &a, const T &b) // fn template definition for max<T>
+{
+	if (EQUAL_TEST)
+		std::cout << AC_YELLOW << "(" << (a < b ? &a : &b) << ") " << AC_NORMAL << '\t';
+	return (a > b ? a : b);
+}
+
+#endif
---
+++ ex00/src/main.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maiman-m <maiman-m@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 08:27:08 by maiman-m          #+#    #+#             */
+/*   Updated: 2024/04/19 10:26:56 by maiman-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+/*
+ * template params/args <>
+ * function params/args ()
+ *
+ * invoking template ::swap<int>
+ * compiler instantiates the template to create a function
+ * the argument provided substitutes all template instances' parameter
+ * invoking function returned by the template with function args ::swap<int>(4, 10)a
+ *
+ * ::swap(4, 10) makes use of template argument deduction because the compiler can infer the template argument type to support the invocation
+ * deduction is not possible if different types are provided as function args
+ *
+ * multi template types and default args can be achieved with templates
+ */
+
+int main(void)
+{
+	FORMAT_TEST("SUBJECT PDF TEST");
+	int a = 2;
+	int b = 3;
+
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+	std::cout << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+	std::cout << std::endl;
+
+	if (EQUAL_TEST)
+	{
+		FORMAT_TEST("RETURN SECOND IF EQUAL TEST");
+		float x = 1.0f;
+		float y = 1.0f;
+
+		std::cout << "address of x: " << &x << std::endl
+				  << "address of y: " << &y << std::endl
+				  << "address of min(x, y) should be equal to that of y: " << ::min(x, y) << std::endl
+				  << "address of max(y, x) should be equal to that of x: " << ::max(y, x) << std::endl;
+		}
+	}
