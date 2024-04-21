ac4d8870a8aa1e9aa47df7f681bcefd561cd973e
isabella <115096194+sevenf0ld@users.noreply.github.com>
2024-04-21 12:53:27 +0800

    formatted and commented

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maiman-m <maiman-m@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 12:34:06 by maiman-m          #+#    #+#             */
-/*   Updated: 2024/04/20 15:47:33 by maiman-m         ###   ########.fr       */
+/*   Updated: 2024/04/21 12:51:15 by maiman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,51 +21,51 @@ int test_subject_file(void)
 	flag = 0;
 	FORMAT_TEST("SUBJECT PDF TEST");
 
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	Array<int> numbers(MAX_VAL);
+	int *mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;
-    return 0;
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+	delete[] mirror;
+	return 0;
 }
 
 void print_element(size_t size, Array<double> const &obj)
@@ -85,7 +85,7 @@ void intro(const char *format, size_t size, Array<double> const &obj)
 	flag = 0;
 
 	FORMAT_TEST(format);
-	std::cout << AC_YELLOW << "size of obj passed in as param is " << size << "." << AC_NORMAL << std::endl;
+	std::cout << AC_YELLOW << "size of obj passed as param is " << size << "." << AC_NORMAL << std::endl;
 	std::cout << "elements of obj passed in as param is ";
 	print_element(size, obj);
 	std::cout << std::endl;
@@ -96,7 +96,7 @@ void test_default(void)
 	flag = 0;
 	FORMAT_TEST("DEFAULT CONSTRUCTOR TEST");
 
-	Array<double>def;
+	Array<double> def;
 	std::cout << std::endl;
 
 	try
@@ -108,7 +108,6 @@ void test_default(void)
 	{
 		ACCESS_ERR(e.what());
 	}
-	std::cout << std::endl;
 }
 
 void test_param(Array<double> const &obj)
@@ -129,7 +128,6 @@ void test_param(Array<double> const &obj)
 
 	std::cout << "accessing element at index " << ARR_SIZE - 1 << " gives the value "
 			  << obj[ARR_SIZE - 1] << std::endl;
-	std::cout << std::endl;
 }
 
 void test_copy(Array<double> &obj)
@@ -139,8 +137,10 @@ void test_copy(Array<double> &obj)
 
 	obj[2] = 1;
 	obj[3] = 2;
+	std::cout << "elements of obj passed as param upon changing element at index 2 and 3: ";
+	print_element(obj.size(), obj);
 
-	const Array<double>copy(obj);
+	const Array<double> copy(obj);
 	std::cout << std::endl;
 
 	std::cout << "elements of copy is ";
@@ -151,9 +151,8 @@ void test_copy(Array<double> &obj)
 	flag = 1;
 	std::cout << "reading from (const) element at index 2..." << std::endl
 			  << "copy[2]: " << AC_CYAN << copy[2] << AC_NORMAL << std::endl;
-	std::cout << std::endl;
 	// error: assignment of read-only location
-	//copy[2] = 3;
+	// copy[2] = 3;
 }
 
 void test_assign(Array<double> &obj)
@@ -163,8 +162,10 @@ void test_assign(Array<double> &obj)
 
 	obj[2] = 3;
 	obj[3] = 4;
+	std::cout << "elements of obj passed as param upon changing element at index 2 and 3: ";
+	print_element(obj.size(), obj);
 
-	Array<double>assign;
+	Array<double> assign;
 	assign = obj;
 	std::cout << std::endl;
 
@@ -175,7 +176,7 @@ void test_assign(Array<double> &obj)
 	FORMAT_TEST("NON-CONST TEST");
 	flag = 1;
 	std::cout << "reading from (non-const) element at index 2..." << std::endl
-			  << "assign[2]: "  << AC_CYAN << assign[2] << AC_NORMAL << std::endl;
+			  << "assign[2]: " << AC_CYAN << assign[2] << AC_NORMAL << std::endl;
 	std::cout << std::endl;
 
 	assign[2] = 5;
@@ -186,7 +187,6 @@ void test_assign(Array<double> &obj)
 
 	std::cout << "elements of assign is updated to ";
 	print_element(assign.size(), assign);
-	std::cout << std::endl;
 }
 
 void test_ctor(void)
@@ -195,23 +195,23 @@ void test_ctor(void)
 	test_default();
 	std::cout << std::endl;
 
-	//Array<double>param(ARR_SIZE);
+	Array<double> param(ARR_SIZE);
 	std::cout << std::endl;
 
-	//test_param(param);
+	test_param(param);
 	std::cout << std::endl;
 
-	//test_copy(param);
+	test_copy(param);
 	std::cout << std::endl;
 
-	//test_assign(param);
+	test_assign(param);
 }
 
 int main(void)
 {
-	//test_subject_file();
+	test_subject_file();
 	std::cout << std::endl;
 
-	test_ctor();
-	std::cout << std::endl;
+	// test_ctor();
+	// std::cout << std::endl;
 }
